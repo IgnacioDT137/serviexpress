1dce7a5d331596fc0dff7ceca6ace10e8169696f
Ignacio Diaz <ign.diaz@duocuc.cl>
2022-11-22 03:18:41 -0300

    conexion

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -1,10 +1,5 @@
 import { Sequelize } from "sequelize";
 
 export const conexion = new Sequelize({
-    dialect: "mysql", 
-    host: "us-cdbr-east-06.cleardb.net",
-    port: 3306,
-    database: "heroku_bdccc5606f2bf19",
-    username: "bf02de5bfeacfb",
-    password: "a6376203"
+    // data
 })
